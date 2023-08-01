57f4741d5033dbc9f69067f13489a7b50810ecf6
University <university@AA687692-Mac.local>
2023-08-01 18:09:29 -0500

    added thought model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const {Schema, model } = require("mongoose");
+
+const usersSchema = new Schema(
+    {
+       username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+       },
+       email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Nice try! but Invalid."]
+       }, 
+       thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }
+       ]
+    }
+)
