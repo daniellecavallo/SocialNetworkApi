10fd1a46a04b23e7f662adab1735ee69a5f2b617
University <university@AA687692-Mac.local>
2023-08-01 22:03:43 -0500

    added thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const {Schema, model } = require("mongoose");
+const reactionsSchema = require("./Reaction")
 
 
 const thoughtsSchema = new Schema(
@@ -9,5 +10,29 @@ const thoughtsSchema = new Schema(
             minlength: 1,
             maxlength: 280
         },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: time => new Date(time).toLocaleTimeString()
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionsSchema]
+
+    },
+   {
+    toJSON: {
+        getters: true
     }
+   }
 )
+
+usersSchema.virtual("reactionsCount").get(function () {
+    return this.reactions.length;
+})
+
+const Thought = model("Thought", thoughtsSchema)
+
+module.exports = Thought
